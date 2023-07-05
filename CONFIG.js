bd3ab71eac8d00f259a2e7590bfe0d2a492d4b02
nam5454 <138707355+nam5454@users.noreply.github.com>
2023-07-06 00:51:09 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Cậu Hết Giận Chưa Á!",
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Cậu Hết Dỗi Chưa Á!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã Hết Giận.",
+    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã Hết Dỗi.",
   buttonYes: "Đã Hết Rồi",
   buttonNo: "Chưa Còn Lâu",
   titleModar: " Ok cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã Hết Giận Nhé. </br> Tớ Không Trêu Thế Nữa. </br> Chúc Cậu Sáng Làm Việc Vui Vẻ Nha.</br> haha <3",
+    "Cảm ơn cậu đã Hết Dỗi Nhé. </br> Tớ Không Trêu Thế Nữa. </br> Chúc Cậu Sáng Làm Việc Vui Vẻ Nha.</br> haha <3",
 };
