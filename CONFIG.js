af7022e0c54020c8f845f137433e6f894152dbfe
nam5454 <138707355+nam5454@users.noreply.github.com>
2023-07-06 00:47:35 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Cậu Hết Giận Chưa Á!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã Hết Giận.",
+  buttonYes: "Đã Hết Rồi",
+  buttonNo: "Chưa Còn Lâu",
+  titleModar: " Ok cậu.",
+  descriptionModar:
+    "Cảm ơn cậu đã Hết Giận Nhé. </br> Tớ Không Trêu Thế Nữa. </br> Chúc Cậu Sáng Làm Việc Vui Vẻ Nha.</br> haha <3",
+};
